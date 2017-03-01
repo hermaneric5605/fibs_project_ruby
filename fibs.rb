498542f1b77fcf0661b23039be69ac654250b6a6
hermaneric5605 <herman7279@gmail.com>
2017-03-01 12:00:21 -0500

    fibs recursion

--- fibs.rb
+++ fibs.rb
@@ -1,3 +1,15 @@
-def fibs(n)
 
+#iteration
+
+def fibs(n, result = [])
+  (0..n).each do |num|
+    if num == 1 || num == 0
+      result << num
+    else
+      result << result[-2] + result[-1]
+    end
+  end
+  print result
 end
+
+#recursion
